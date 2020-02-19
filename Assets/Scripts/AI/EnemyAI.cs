b63015842734b0c94969830eea77101703fecb27
BobaBinks <42332575+tolemwar@users.noreply.github.com>
2020-02-19 20:49:42 +0800

    StateMachine

---
+++ Assets/Scripts/AI/EnemyAI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StateStuff;
+public class EnemyAI : MonoBehaviour
+{
+    private List<playerObject>
+    public StateMachine<EnemyAI> sm {get;set;}
+    // Start is called before the first frame update
+    void Start()
+    {
+        sm = new StateMachine<EnemyAI>(this);
+        sm.AddState(new PatrolState(this, "Patrol"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/Scripts/AI/States/PatrolState.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StateStuff;
+using System;
+
+public class PatrolState : State<EnemyAI>
+{
+    public EnemyAI m_go;
+    public PatrolState(EnemyAI _gameObject, string stateID) 
+        : base(State<EnemyAI>(stateID)) 
+    {
+        m_go = _gameObject;
+    }
+
+    private static string State<T>(string stateID)
+    {
+        return stateID;
+    }
+
+    public override void Enter()
+    {
+        throw new System.NotImplementedException();
+
+    }
+    public override void Update(double dt)
+    {
+        throw new System.NotImplementedException();
+        //m_go.sm.SetNextState("")
+    }
+    public override void Exit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+}
