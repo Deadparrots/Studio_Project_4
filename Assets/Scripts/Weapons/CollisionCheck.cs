75837b95f199dce8082544c6b041d51d2e196c39
BobaBinks <42332575+tolemwar@users.noreply.github.com>
2020-03-04 12:44:15 +0800

    Fixed some stuff for enemy

--- Assets/Scripts/Weapons/CollisionCheck.cs
+++ Assets/Scripts/Weapons/CollisionCheck.cs
@@ -40,7 +40,7 @@ public class CollisionCheck : MonoBehaviour
                 // Destroy Bullet
                 Server_Demo.Instance.DestroyBullet(bulletManager.pid);
             }
-            else if (collision.gameObject.name == "MeleeEnemyAI(Clone)")
+            else if (collision.gameObject.name == "MeleeEnemyAI(Clone)" || collision.gameObject.name == "MeleeEnemyAI")
             {
                 // Deal dmg to enemy
                 EnemyAI enemy = collision.gameObject.GetComponent<EnemyAI>();
